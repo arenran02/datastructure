f9496236943247e38711c7c30416814be5775e8a
arenran02 <arenran02@gmail.com>
2024-04-15 09:40:46 +0900

    update: 2024-04-15

--- week7/2.c
+++ week7/2.c
@@ -1,144 +1,164 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// 다항식의 각 항을 표현하는 노드 구조체
+// 집합의 각 원소를 표현하는 노드 구조체
 struct Node {
     int num;
     struct Node* next; // 다음 노드를 가리키는 포인터
 };
 
-// 다항식 출력 함수
-void printList(struct Node* node) {
+// 집합 출력 함수
+void printSet(struct Node* node) {
+    if (node == NULL) {
+        printf("0\n");
+    }
+
     while (node != NULL) {
         printf("%d ", node->num);
         node = node->next;
     }
+
     printf("\n");
 }
 
-// 다항식에 항 추가 함수
-void addTerm(struct Node** a, int n) {
+// 집합에 원소 추가 함수 (직접 구현)
+void addElement(struct Node** set, int n) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->num = n;
     newNode->next = NULL;
 
-    // 다항식이 비어있을 경우
-    if (*a == NULL) {
-        *a = newNode;
+    // 집합이 비어있을 경우
+    if (*set == NULL) {
+        *set = newNode;
     } else {
-        struct Node* temp = *a;
-        // 마지막 노드까지 이동
-        while (temp->next != NULL) {
+        struct Node* temp = *set;
+        struct Node* prev = NULL; // 이전 노드를 추적하기 위한 포인터
+
+        // 새로운 원소가 적절한 위치에 삽입될 때까지 노드를 순회
+        while (temp != NULL && temp->num < n) {
+            prev = temp;
             temp = temp->next;
         }
-        // 새로운 노드를 마지막 노드의 다음에 추가
-        temp->next = newNode;
+
+        // 새로운 원소를 적절한 위치에 삽입
+        if (prev == NULL) {
+            // 새로운 원소가 첫 번째 원소보다 작은 경우
+            newNode->next = *set;
+            *set = newNode;
+        } else {
+            // 새로운 원소가 중간에 삽입되는 경우
+            prev->next = newNode;
+            newNode->next = temp;
+        }
     }
 }
 
-struct Node* UnionNode(struct Node* a, struct Node* b){
-    struct Node* temp=NULL;
-
-    // 두 다항식이 모두 비어있는 경우
-    if (a == NULL && b == NULL)
-        return NULL;
-
-    // 다항식 b가 비어있는 경우
-    if (b == NULL&&a!=NULL)
-        return a;
-
-    // 다항식 a가 비어있는 경우
-    if (a == NULL&&b!=NULL)
-        return b;
-
-    // 다항식 a를 순회하면서 각 항이 다항식 b에 포함되어 있는지 확인
-    struct Node* tempA = a;
-    while (tempA != NULL) {
-        struct Node* tempB = b;
-        int found = 0; // 다항식 b에 현재 항이 존재하는지 여부를 나타내는 플래그
-        while (tempB != NULL) {
-            if (tempA->num == tempB->num) {
-                found = 1; // 다항식 b에 현재 항이 존재함
-                addTerm(&temp, tempA->num);
-                break;
-            }
-            tempB = tempB->next;
-        }
-        // 다항식 b에 현재 항이 존재하지 않으면 subset이 아님
-        if (!found)
-            addTerm(&temp, tempA->num);
-        tempA = tempA->next;
+// 집합의 메모리 해제 함수
+void freeSet(struct Node* set) {
+    while (set != NULL) {
+        struct Node* temp = set;
+        set = set->next;
+        free(temp);
     }
-
-    return temp;
 }
 
-struct Node* InterNode(struct Node* a, struct Node* b){
-    struct Node* temp=NULL;
+// 집합에 해당 요소가 있는지 확인하는 함수
+int isElementInSet(struct Node* set, int n) {
+    while (set != NULL) {
+        if (set->num == n)
+            return 1; // 요소가 이미 집합에 존재함
+        set = set->next;
+    }
+    return 0; // 요소가 집합에 없음
+}
 
-    // 두 다항식이 모두 비어있는 경우
+// 두 집합의 합집합 구하는 함수 (직접 구현)
+struct Node* UnionSet(struct Node* a, struct Node* b) {
+    struct Node* result = NULL;
+
+    // 두 집합이 모두 비어있는 경우
     if (a == NULL && b == NULL)
         return NULL;
 
-    // 다항식 b가 비어있는 경우
-    if (b == NULL&&a!=NULL)
-        return NULL;
-
-    // 다항식 a가 비어있는 경우
-    if (a == NULL&&b!=NULL)
-        return NULL;
-
-    // 다항식 a를 순회하면서 각 항이 다항식 b에 포함되어 있는지 확인
-    struct Node* tempA = a;
-    while (tempA != NULL) {
-        struct Node* tempB = b;
-        int found = 0; // 다항식 b에 현재 항이 존재하는지 여부를 나타내는 플래그
-        while (tempB != NULL) {
-            if (tempA->num == tempB->num) {
-                found = 1; // 다항식 b에 현재 항이 존재함
-                addTerm(&temp, tempA->num);
-                break;
-            }
-            tempB = tempB->next;
-        }
-        // 다항식 b에 현재 항이 존재하지 않으면 subset이 아님
-        if (!found)
-            tempA = tempA->next;
+    // 집합 a를 순회하면서 각 원소를 결과 집합에 추가
+    while (a != NULL) {
+        // 이미 있는 요소는 추가하지 않음
+        if (!isElementInSet(result, a->num))
+            addElement(&result, a->num);
+        a = a->next;
     }
-    return temp;
-}  
 
-int main(){
-    struct Node* MyNode1 = NULL;
-    struct Node* MyNode2 = NULL;
+    // 집합 b를 순회하면서 결과 집합에 추가
+    while (b != NULL) {
+        // 이미 있는 요소는 추가하지 않음
+        if (!isElementInSet(result, b->num))
+            addElement(&result, b->num);
+        b = b->next;
+    }
 
-    // 첫 번째 다항식의 크기를 입력 받음
+    return result;
+}
+
+
+// 두 집합의 교집합 구하는 함수
+struct Node* IntersectSet(struct Node* a, struct Node* b) {
+    struct Node* tempA = a;
+    struct Node* result = NULL;
+
+    // 두 집합이 모두 비어있는 경우
+    if (a == NULL && b == NULL)
+        return NULL;
+
+    // 집합 b를 순회하면서 각 원소를 결과 집합에 추가
+    while (b != NULL) {
+        // 이미 있는 요소는 추가하지 않음
+        if (isElementInSet(tempA, b->num))
+            addElement(&result, b->num);
+        b = b->next;
+    }
+
+    return result;
+}
+
+int main() {
+    struct Node* set1 = NULL;
+    struct Node* set2 = NULL;
+
+    // 첫 번째 집합의 크기를 입력 받음
     int num1;
     scanf("%d", &num1);
-    int temp = 0;
 
-    if (num1 != 0){
-        // 첫 번째 다항식의 항을 입력 받음
+    if (num1 != 0) {
+        // 첫 번째 집합의 원소를 입력 받음
         for (int i = 0; i < num1; i++) {
-            scanf("%d ", &temp);
-            addTerm(&MyNode1, temp);
+            int temp;
+            scanf("%d", &temp);
+            addElement(&set1, temp);
         }
     }
 
-    // 두 번째 다항식의 크기를 입력 받음
+    // 두 번째 집합의 크기를 입력 받음
     int num2;
     scanf("%d", &num2);
 
-    if (num2 != 0){
-        // 두 번째 다항식의 항을 입력 받음
+    if (num2 != 0) {
+        // 두 번째 집합의 원소를 입력 받음
         for (int i = 0; i < num2; i++) {
-            scanf("%d ", &temp);
-            addTerm(&MyNode2, temp);
+            int temp;
+            scanf("%d", &temp);
+            addElement(&set2, temp);
         }
     }
 
-    printList(UnionNode(MyNode1, MyNode2));
-    printList(InterNode(MyNode1, MyNode2));
+    // 합집합 출력
+    printSet(UnionSet(set1, set2));
+    
+    //교집합 출력
+    printSet(IntersectSet(set1, set2));
+
+    // 집합 메모리 해제
+    freeSet(set1);
+    freeSet(set2);
 
     return 0;
 }
